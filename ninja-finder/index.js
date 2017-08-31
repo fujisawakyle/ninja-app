831e281d738164048bf4999895c0d62b99b0cf93
fujisawakyle <fujisawakyle@gmail.com>
2017-08-31 08:34:11 -0700

    initial commit

---
+++ ninja-finder/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const routes = require('./routes/api');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// set up express app
+const app = express();
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+//mongoose promise is deprecated so need to set to global promise.
+mongoose.Promise = global.Promise;
+
+
+//middleware
+//must before our route init
+app.use(bodyParser.json());
+
+
+//uses routes from api.js file
+// '/api' puts that in the url before any of the routes
+app.use('/api',routes);
+
+//error handling middleware
+app.use(function(err, req, res, next){
+  //console.log(err);
+  res.status(422).send({error: err.message})
+});
+
+//listen for requests
+app.listen(process.env.port || 4000, function () {
+  console.log('now listening for requests');
+});
---
+++ ninja-finder/routes/api.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const Ninja = require('../models/ninja')
+
+// get a list of ninjas from the db
+router.get('/ninjas', function(req, res, next) {
+  res.send({type:'GET'});
+});
+
+// add a new ninja to the db
+router.post('/ninjas', function(req, res, next) {
+  //creates a new instance and saves it
+  Ninja.create(req.body).then(function(ninja){
+    //good practice to send back the data
+    res.send(ninja);
+  }).catch(next);
+
+});
+
+// update a ninja in the db
+router.put('/ninjas/:id', function(req, res, next) {
+  Ninja.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+    //this above line updates the body, but you have to call it again to find updated version of ninja instance.
+    Ninja.findOne({_id: req.params.id}).then(function(ninja){
+      res.send(ninja);
+    });
+  });
+});
+
+// delete a ninja in the db
+router.delete('/ninjas/:id', function(req, res, next) {
+  //mongoose delete method
+  Ninja.findByIdAndRemove({_id: req.params.id}).then(function(ninja){
+    res.send(ninja);
+  });
+});
+
+module.exports = router;
